6d16b28db7176697046ea738d357906b924d0a0c
Kate Galati <kategalati@Kates-MacBook-Air.local>
2022-09-19 20:43:54 -0700

    commit tree eadf3582462318108c7988713c550d6cdc70daeb

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if (rideLength <= 400) {
+    return "This one is on me!";
+  }
+    else if (rideLength <= 2000) {
+      return "That will be twenty bucks."
+    }
+      else if (rideLength > 2000 && rideLength <= 2500) {
+        return "I will gladly take your thirty bucks."
+      }
+        else {
+          return "No can do."
+        }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return (city === 'NYC' ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipType) {
+  let tipResponse;
+
+  switch (tipType) {
+    case "generous":
+      tipResponse = "Thank you so much.";
+      break;
+    case "not as generous":
+      tipResponse = "Thank you.";
+      break;
+    default:
+      tipResponse = "Bye.";
+  }
+
+  return tipResponse;
 }
